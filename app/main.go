e81b3fc02739282b2bbe63d7c1d0d3afb298010f
teymur huseynov <thuseynovint@gmail.com>
2022-05-05 15:41:57 +0200

    added new feature

---
+++ app/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+/*
+ * Here we statically load the static.json
+ * file into the compiled binary
+ */
+
+var staticFs embed.FS
+
+const port = 8080
+
+func main() {
+	http.Handle("/", http.FileServer(http.FS(staticFs)))
+
+	log.Printf("Listening on :%d...\n", port)
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+		log.Fatal(err)
+	}
+}
